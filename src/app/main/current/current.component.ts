ada879ea28c935e2b84a305233315815cfff00dd
ftedo <farago.teodora@gmail.com>
2021-02-06 23:36:06 +0100

    magic animation

--- src/app/main/current/current.component.ts
+++ src/app/main/current/current.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-current',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current.component.css']
 })
 export class CurrentComponent implements OnInit {
-
+  isVideoPlaying = true;
+  isVideoPlayable = true;
   constructor() { }
 
   ngOnInit(): void {
+    this.isVideoPlayable = window.innerWidth > 500 
   }
 
+  onBackgroundLoaded() {
+    this.isVideoPlaying = window.innerWidth > 500 
+  }
+
+  onVideoEnded() {
+    setTimeout(() => {
+      this.isVideoPlaying = false;
+    }, 500)
+  }
 }
